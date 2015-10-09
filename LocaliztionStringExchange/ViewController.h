d1fa6a7758704dfe7ac4389b855b55d8bc1a1df9
NY <nycode.jn@gmail.com>
2015-10-09 15:12:52 +0800

    Initial Commit

---
+++ LocaliztionStringExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocaliztionStringExchange
+//
+//  Created by 倪瑶 on 15/10/9.
+//  Copyright © 2015年 nycode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocaliztionStringExchange/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocaliztionStringExchange
+//
+//  Created by 倪瑶 on 15/10/9.
+//  Copyright © 2015年 nycode. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
